047e6a5dd9f162e954426e28702c134de22be74d
ElderLucas <elderlucas@gmail.com>
2015-12-10 11:12:04 +0000

    Setup to Build a Database

--- database_setup.py
+++ database_setup.py
@@ -1,69 +1,100 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, Unicode
+from sqlalchemy import Column, ForeignKey, Integer, String, Unicode, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.dialects.mysql import TIMESTAMP
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
+from datetime import datetime
+from sqlalchemy.sql import func
+
+from sqlalchemy.dialects.mysql import INTEGER
+
 import sqlalchemy
- 
+
 Base = declarative_base()
 
-DBpath = "mysql://root:oigalera8458@localhost/restaurant_utf8"
+DBpath = "mysql://root:oigalera8458@localhost/catolicosapp_utf8"
 
 engine = sqlalchemy.create_engine('mysql://root:oigalera8458@localhost')
 
-#engine.execute("CREATE DATABASE restaurant_utf8 CHARACTER SET utf8 COLLATE utf8_general_ci")
+#engine.execute("CREATE DATABASE catolicosapp_utf8 CHARACTER SET utf8 COLLATE utf8_general_ci")
 
 class User(Base):
-    __tablename__ = 'user' 
+	__tablename__ = 'user'
+	id 				= Column(Integer, primary_key=True)
+	name 			= Column(Unicode(250, collation='utf8_bin'), nullable=False)
+	email 			= Column(Unicode(250, collation='utf8_bin'), nullable=False)
+	url_picture 	= Column(Unicode(250, collation='utf8_bin'))
+	phone_number 	= Column(Unicode(250, collation='utf8_bin'))
+	sex 			= Column(Unicode(250, collation='utf8_bin'))
+	bio 			= Column(Unicode(250, collation='utf8_bin'))
+	web_page 		= Column(Unicode(250, collation='utf8_bin'))
+	city 			= Column(Unicode(250, collation='utf8_bin'))
+	country 		= Column(Unicode(250, collation='utf8_bin'))
+	address 		= Column(Unicode(250, collation='utf8_bin'))
+	old 			= Column(Unicode(250, collation='utf8_bin'))
 
-    id = Column(Integer, primary_key=True)
-    name = Column(Unicode(250, collation='utf8_bin'), nullable=False)
-    email = Column(Unicode(250, collation='utf8_bin'), nullable=False)
-    picture = Column(Unicode(250, collation='utf8_bin'))
+	@property
+	def serialize(self):
+		"""Return object data in easily serializeable format"""
+		return {
+			'name'	: self.name,
+			'id'	: self.id,
+		}
 
 
-class Restaurant(Base):
-    __tablename__ = 'restaurant'
-   
-    id = Column(Integer, primary_key=True)
-    name = Column(Unicode(250, collation='utf8_bin'), nullable=False)
-    user_id = Column(Integer,ForeignKey('user.id'))
-    user = relationship(User)
+class Following(Base):
+	__tablename__ = 'following'
+	id 					= Column(Integer, primary_key=True)
+	user_id 			= Column(Integer,ForeignKey('user.id'))
+	following_user_id 	= Column(Integer,ForeignKey('user.id'))
+	current_time 		= Column(TIMESTAMP, server_default= func.now(), onupdate=func.current_timestamp())
 
 
-    @property
-    def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'name'         : self.name,
-           'id'           : self.id,
-       }
- 
-class MenuItem(Base):
-    __tablename__ = 'menu_item'
+class Post(Base):
+	__tablename__ = 'post'
+	id 					= Column(Integer, primary_key=True)
+	user_id 			= Column(Integer,ForeignKey('user.id'))
+	post_current_time 	= Column(TIMESTAMP, server_default=func.now(), onupdate=func.current_timestamp())
+	post_text 			= Column(Unicode(250, collation='utf8_bin'))
+	post_url_picture 	= Column(Unicode(250, collation='utf8_bin'))
+	post_url_video 		= Column(Unicode(250, collation='utf8_bin'))
 
-    name =Column(Unicode(250, collation='utf8_bin'), nullable = False)
-    id = Column(Integer, primary_key = True)
-    description = Column(Unicode(250, collation='utf8_bin'))
-    price = Column(Unicode(250, collation='utf8_bin'))
-    course = Column(Unicode(250, collation='utf8_bin'))
-    restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
-    restaurant = relationship(Restaurant)
-    user_id = Column(Integer,ForeignKey('user.id'))
-    user = relationship(User)
 
-    @property
-    def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'name'         : self.name,
-           'description'  : self.description,
-           'id'           : self.id,
-           'price'        : self.price,
-           'course'       : self.course,
-       }
+class nlike(Base):
+	__tablename__ = 'nlike'
+	id 					= Column(Integer, primary_key=True)
+	user_id 			= Column(Integer,ForeignKey('user.id'))
+	post_id 			= Column(Integer,ForeignKey('post.id'))
+	like_current_time 	= Column(TIMESTAMP, server_default=func.now(), onupdate=func.current_timestamp())
 
-####### Nao esquecer de mudar a senha do data base aqui.
+
+class Comment(Base):
+	__tablename__ = 'comment'
+	id 						= Column(Integer, primary_key=True)
+	user_id					= Column(Integer,ForeignKey('user.id'))
+	post_id					= Column(Integer,ForeignKey('post.id'))
+	comment_text            = Column(Unicode(250, collation='utf8_bin'))
+	comment_current_time 	= Column(TIMESTAMP, server_default=func.now(), onupdate=func.current_timestamp())
+
+
+class Pray(Base):
+	__tablename__ = 'pray'
+	id 					= Column(Integer, primary_key=True)
+	user_id 			= Column(Integer,ForeignKey('user.id'))
+	post_id 			= Column(Integer,ForeignKey('post.id'))
+	pray_current_time 	= Column(TIMESTAMP, server_default=func.now(), onupdate=func.current_timestamp())
+
+
+class Share(Base):
+	__tablename__ = 'share'
+	id 					= Column(Integer, primary_key=True)
+	user_id 			= Column(Integer,ForeignKey('user.id'))
+	post_id 			= Column(Integer,ForeignKey('post.id'))
+	share_current_time 	= Column(TIMESTAMP, server_default=func.now(), onupdate=func.current_timestamp())
+
 engine = create_engine(DBpath)
 
 Base.metadata.create_all(engine)
+
+
